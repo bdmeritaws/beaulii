e0dd9a61a9e14672e570e699aec032bf026f98ce
nasim5188 <nasim.ahamed411@gmail.com>
2026-04-10 20:03:56 +0600

    update s3.js file

--- src/lib/s3.js
+++ src/lib/s3.js
@@ -2,15 +2,15 @@ import { S3Client, PutObjectCommand, DeleteObjectCommand } from "@aws-sdk/client
 import { v4 as uuidv4 } from "uuid";
 
 const s3Client = new S3Client({
-  region: process.env.AWS_REGION || "us-east-1",
+  region: process.env.AWS_REGION1 || "us-east-1",
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID1,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY1,
   },
 });
 
-const BUCKET_NAME = process.env.AWS_S3_BUCKET_NAME || "beaulii";
-const CDN_URL = process.env.AWS_CDN_URL || `https://${BUCKET_NAME}.s3.${process.env.AWS_REGION || "us-east-1"}.amazonaws.com`;
+const BUCKET_NAME = process.env.AWS_S3_BUCKET_NAME1 || "beaulii";
+const CDN_URL = process.env.AWS_CDN_URL1 || `https://${BUCKET_NAME}.s3.${process.env.AWS_REGION1 || "us-east-1"}.amazonaws.com`;
 
 const ALLOWED_TYPES = {
   "image/jpeg": "jpg",
